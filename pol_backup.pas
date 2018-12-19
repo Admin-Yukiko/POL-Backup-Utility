307832c58481a91bd3322a18ea3038f99bbdd0cf
Admin\Me <hopelivesproject@gmail.com>
2018-12-19 00:09:32 -0600

    Added comments to the source. Removed unused variables.

--- pol_backup.pas
+++ pol_backup.pas
@@ -87,18 +87,17 @@ const
 var
   POLBackupper: TPOLBackupper;
   TimerRunning: Boolean = False;
-	Schedule : Array[0..6] of Integer;
-  TempSched: Array[0..6] of Integer;
-  NumDays: Integer;												// The number of days backups are scheduled. Can be 1-7
-  StartDay: Integer;											// The numbered day of the week the timer was started.
-  FoundIt: Boolean;
-  I: Integer;
 
 implementation
 
 /////////////////////////////////////////////////
+//
 // Looks for the presence of transitional
-// *.ndt files in POL data folder.
+// *.ndt files in POL data folder. These are
+// files created by POL during a world save.
+// Once the world save is complete POL renames
+// the *.ndt files to *.txt.
+//
 /////////////////////////////////////////////////
 function NDTFilesPresent(SourceFolder: String) : Boolean;
 
@@ -107,11 +106,21 @@ Var
 	FoundFiles: TStringList;
 Begin
 	FoundFiles := FindAllFiles(SourceFolder, '*.ndt', True);
+  
+  // If count is greater than 0 then there are *.ndt files in the POL /data directory.
   If(FoundFiles.Count > 0) Then NDTsPresent := True
   Else NDTsPresent := False;
   Result := NDTsPresent;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// This procedure does the zip compression of the directory
+// and directories under the target directory and places it
+// in the back-up directory specified. It is called by the
+// IsAScheduledDay function.
+//
+///////////////////////////////////////////////////////////////
 procedure ZipTheDir();
 
 var
@@ -168,40 +177,71 @@ begin
   end;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// This function is called by WaitForTime and returns if the
+// checkbox for a day is not checked. If it is checked then
+// it waits for the proper time. It then calls the
+// NDTFilesPresent function until there are no longer any
+// *.ndt files present in the directory to be backed-up.
+// Then it calls ZipTheDir.
+//
+///////////////////////////////////////////////////////////////
 Function IsAScheduledDay(DayCheck: Boolean): Boolean;
 
 Var
   CurrentTime: Integer;
   BackupTime: Integer;
 Begin
+  // If the checkbox for the current day is not checked then exit back to WaitForTime
   If(Not DayCheck) Then Exit;
+  
+  // Get the current time in hours and minutes.
   CurrentTime := Numb2Dec(FormatDateTime('hhnn',Now), 10);
+  
+  // Get the backup time from TimeEdit1, remove the semicolon, and convert it to a decimal integer.
   BackupTime := Numb2Dec(ReplaceStr(POLBackupper.TimeEdit1.Text, ':', ''), 10);
+  
+  // If it is already past the backup time then return to WaitForTime.
+  // This is necessary if the user starts the backup timer on a day she has selected
+  // but later than the time scheduled backup time.
   If(CurrentTime > BackupTime) Then Exit;
+  
+  // Now we wait while the current time is not equal to the backup time.
   While(Numb2Dec(FormatDateTime('hhnn',Now), 10) <> BackupTime) Do
   Begin
     If(Not TimerRunning) Then Exit;
     Application.ProcessMessages;
     Sleep(100);
   end;
+  
+  // Ok. The current time equals the backup time.
   // Check for *.ndt files. When they are gone exit the loop.
   While(NDTFilesPresent(POLBackupper.SourceDirEdit.Text + '/')) Do
     Begin
       Sleep(50);
       Application.ProcessMessages;
     end;
-
+	
+  //Do the backup.
   ZipTheDir();
 
 end;
 
-{	procedure WaitForTime(BackupTime: String);
-	BackupTime is the time set by the user to backup the POL directory.
+///////////////////////////////////////////////////////////////
+// procedure WaitForTime(BackupTime: String);
+// BackupTime is the time set by the user to backup the POL
+// directory.
+//
+// TimerRunning is a boolean that is set to true by the Start Timer
+// button just prior to this procedure being called. The Stop Timer
+// button sets it to false.
+//
+// This is truly the core function, along with its support
+// procedures and functions, that does the bulk of the work.
+//
+///////////////////////////////////////////////////////////////
 
-	TimerRunning is a boolean that is set to true by the Start Timer
-	button just prior to this procedure being called. The Stop Timer
-	button sets it to false.
-}
 procedure WaitForTime();
 
 Var
@@ -213,6 +253,7 @@ Begin
     Begin
 		  CurrentDay := FormatDateTime('dddd',Now);
 
+      // Goes through the days looking for the next one that has its checkbox checked.
 		  Case(CurrentDay) of
   	    'Sunday':			IsAScheduledDay(POLBackupper.SundayCheckBox.Checked);
   	    'Monday':			IsAScheduledDay(POLBackupper.MondayCheckBox.Checked);
@@ -222,7 +263,10 @@ Begin
   	    'Friday':			IsAScheduledDay(POLBackupper.FridayCheckBox.Checked);
   	    'Saturday':		IsAScheduledDay(POLBackupper.SaturdayCheckBox.Checked);
       end;
+      
       If(Not TimerRunning) Then Exit;
+      
+      // Here we wait for the current day to change to the next day.
       Repeat
         Sleep(100);
         If(Not TimerRunning) Then Exit;
@@ -236,9 +280,13 @@ end;
 
 { TPOLBackupper }
 
+///////////////////////////////////////////////////////////////
+//
 // Bulk enable or disable checkboxes and certain other things
 // during the time the scheduler is active.
 // EnableThem - True to Enable, False to disable.
+//
+///////////////////////////////////////////////////////////////
 procedure EnableDaysCheckBoxes(EnableThem: Boolean);
 
 Begin
@@ -258,6 +306,15 @@ Begin
 
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// The procedure for the POL directory browse butto.
+// Used to select a POL server location should the user want
+// to run POL from this program.
+//
+// It ensures the user has selected a valid POL directory.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.POLDirBrowseBtnClick(Sender: TObject);
 
 Var
@@ -276,6 +333,11 @@ begin
 
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// The procedure for the Start Backup button.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.StartBtnClick(Sender: TObject);
 
 Var
@@ -292,18 +354,27 @@ begin
       ShowMessage('Please select a back-up location.');
       Exit;
     end;
-
+	// Set TimerRunning to True.
   TimerRunning := True;
+  // Displays the text in the status bar.
   StatusBar1.SimpleText:='    Status of the Backup Scheduler:    Running.';
+  // Disables various things whilst the backup timer is running.
   SourceDirBtn.Enabled:=False;
   DestDirBtn.Enabled:=False;
   StartBtn.Enabled:=False;
   TimeEdit1.Enabled:=False;
   BackupFileNameEdit.Enabled:=False;
   EnableDaysCheckBoxes(False);
+  // The WaitForTime function is called.
   WaitForTime();
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// Procedure for the browse button to choose the directory
+// to backup.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.SourceDirBtnClick(Sender: TObject);
 
 Var
@@ -314,6 +385,12 @@ begin
   if SelectDirectoryDialog1.Execute then SourceDirEdit.Text:=SelectDirectoryDialog1.FileName;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// The procedure that will start the POL server, provided the
+// user has selected a valid POL directory.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.StartPOLBtnClick(Sender: TObject);
 begin
   If(POLDirEdit.Text = '') Then
@@ -323,6 +400,11 @@ begin
   Else ShellExecute(0,nil, PChar('cmd'),PChar('/c ' + 'cd /D ' + POLDirEdit.Text + ' && ' + 'pol.exe'),nil,1);
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// Procedure to stop the backup timer.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.StopBtnClick(Sender: TObject);
 begin
   TimerRunning := False;
@@ -336,6 +418,11 @@ begin
 
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// Procedure to toggle all day's checkboxes.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.ToglAllDaysCheckBoxChange(Sender: TObject);
 begin
   If(ToglAllDaysCheckBox.Checked) Then
@@ -360,6 +447,12 @@ begin
   end;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// Procedure for the browse button to choose a backup
+// location.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.DestDirBtnClick(Sender: TObject);
 Var
 	JunkString: String;
@@ -369,12 +462,23 @@ begin
   if SelectDirectoryDialog1.Execute then DestDirEdit.Text:=SelectDirectoryDialog1.FileName;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// The procedure called when the program is closed.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.FormClose(Sender: TObject; var CloseAction: TCloseAction
   );
 begin
   Halt;
 end;
 
+///////////////////////////////////////////////////////////////
+//
+// This is called when the program is executed.
+// It displays the splash screen.
+//
+///////////////////////////////////////////////////////////////
 procedure TPOLBackupper.FormShow(Sender: TObject);
 begin
   Sleep(100);
